5cf1928fefeb131876ea8196072b92b6cce2fc22
AnteAntonini <aantonini@combis.hr>
2023-10-09 22:20:56 +0200

    remove google font import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import Navbar from "../components/Navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Portfolio App",
@@ -12,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
       </body>
