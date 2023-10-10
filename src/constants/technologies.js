a7184e65ad227e26f2c434589b1ad4a24c11478e
AnteAntonini <aantonini@combis.hr>
2023-10-10 10:43:19 +0200

    add technology constant list

---
+++ src/constants/technologies.js
@@ -0,0 +1,46 @@
+export const technologies = [
+  {
+    name: "HTML 5",
+    icon: "html",
+  },
+  {
+    name: "CSS 3",
+    icon: "css",
+  },
+  {
+    name: "JavaScript",
+    icon: "javascript",
+  },
+  {
+    name: "TypeScript",
+    icon: "typescript",
+  },
+  {
+    name: "React JS",
+    icon: "react",
+  },
+  {
+    name: "Vue JS",
+    icon: "vue",
+  },
+  {
+    name: "Storybook",
+    icon: "storybook",
+  },
+  {
+    name: "SASS",
+    icon: "sass",
+  },
+  {
+    name: "GitHub",
+    icon: "github",
+  },
+  {
+    name: "Git",
+    icon: "git",
+  },
+  {
+    name: "Figma",
+    icon: "figma",
+  },
+];
